2aa144c05272d4fe5b9da01608f6507960b08760
myNewGitAcc <newregwithemail@gmail.com>
2025-08-04 15:06:41 +0300

    update scripts/links order

--- scripts/loadMetatags.js
+++ scripts/loadMetatags.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
   fetch('metatags.html')
     .then((res) => res.text())
     .then((data) => {
-      document.querySelector('head').insertAdjacentHTML('afterbegin', data);
+      document.querySelector('head').insertAdjacentHTML('beforeend', data);
     })
     .catch((err) => {
       console.error('Error loading header:', err);
